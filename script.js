9d71bd0834f9f533d9cc7b265b7aa583caebbbbc
gio98 <gio982cap@live.it>
2024-09-26 10:40:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,38 @@ console.clear();
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
+let myaudio = document.getElementById("myAudio");
 const startBtn = document.querySelector('#start-button');
 let btnOpacity = 1;
 startBtn.style.opacity = btnOpacity;
 let timer = 0;
+const text = [
+  "Feliz aniversario al amor de mi vida, gracias por existir, tú que eres la persona más increíble y extraordinaria de mi vida, que me completa en cada momento y que haces que mi corazón explote de infinita felicidad, que me das una fuerza infinita capaz de hacerme tocar el cielo con un dedo.",
+  "Aquí estamos nuevamente este año con nuestro amor infinito, hemos llegado a nuestro hermoso día que llega como una sorpresa navideña y es un día extraordinario de celebración, como cada día hermoso y único que paso contigo a tu lado.",
+  "Hoy estoy aquí amor de mi vida para decirte gracias inmensamente y lo haré en cada momento de mi vida y más allá por haber entrado en mi vida y por estar aquí a mi lado brindándome todo tu precioso amor, que cada día hace mi vida especial y me ha hecho cada día que pasa una mejor persona.",
+  "Todo esto gracias a ti por la persona increíble que eres con tu dulce sonrisa, que hace que mi corazón lata más rápido y me deja sin aliento, tu infinita dulzura que me llena de felicidad y calma, que me hace sentir segura, lejos de todo mal.",
+  "Tu fuerza infinita que desde la pasión que pones desde tu precioso corazón puedes superar desafíos increíbles completando cada obstáculo que la vida te pone frente, y pones todo de ti en ello luchando con uñas y dientes y todo lo que haces.",
+  "Cada momento entonces se vuelve único porque lo haces aplicando la pasión que llega al fondo de tu precioso corazón y te tomo como ejemplo desde tu grandiosa fuerza, y aún en esos momentos donde no sé qué hacer pienso en ti.",
+  "Abriendo mi corazón y esto nunca me hace parar porque siento que te tengo aquí en mi corazón, unidos en el alma y juntos amor de mi vida somos capaces de afrontar y vencer todo, y sobre todo siempre recuerdas que todos los días eres el número uno en todo el universo.",
+  "Hasta en los momentos más difíciles recuerda siempre que estaré aquí a tu lado, en las buenas y en las malas y quiero estar aquí a tu lado y proteger con todo de mí tu preciosa alma y tu precioso corazón.",
+  "Gracias por cada uno de tus infinitos y cálidos abrazos, donde me siento como un niño envuelto a salvo de todo daño, y me encanta estar aquí todos los días para ser tu almohada mientras te protejo en mis cálidos abrazos.",
+  "Gracias por cada uno de tus besos dulces y preciosos que más que oxígeno ese aliento me da vida y fuerza para levantarme de acostarnos y enfrentar la vida juntos.",
+  "Amo por cada pequeño o gran hito que se combina con cada día increíble e inolvidable para celebrarlo, y sobre todo el amor más especial de mi vida es compartir cada momento contigo y regocijarnos juntos preservando ese momento juntos en nuestros corazones y mentes.",
+  "Gracias por estar aquí y enseñarme cosas extraordinarias cada día, y con la fuerza infinita de nuestro amor mi corazón ha venido a hacerme hacer cosas que sin ti nunca hubiera hecho y lo extraordinario es ver tú feliz por todo lo que hago.",
+  "Lo más lindo y extraordinario para mí es verte feliz y verte sentir bien, porque eso es lo más importante para mí todos los días.",
+  "Felices 3 años juntos amor de mi vida, que este día como cada momento a tu lado sea extraordinario, y que dios proteja nuestra preciosa y extraordinaria relación que cada día se vuelve más increíble y extraordinaria.",
+  "Quiero pasar cada día aquí a tu lado, tú quien eres y por siempre serás mi increíble, dulce y única compañera de vida, y nunca dejaré de decirte gracias por existir aquí a mi lado y por cómo cada día haces que mi vida sea extraordinaria y única.",
+  "Te amo inmensamente desde el fondo de mi corazón que te ama desde el fondo y existe para acogerte aquí en el fondo, donde se encuentra mi alma que se une a la tuya convirtiéndose juntos en una sola persona.",
+  "Te amo infinitamente desde lo más profundo de mi alma y de mi cuerpo, tú que eres mi corazón que como un motor me mantiene vivo para estar aquí a tu lado y protegerte con todo mi amor.",
+  "Tú que eres más importante que el oxígeno que respiro, eres mi cuerpo, mi alma, cada molécula que me compone, tú que eres mi compañera de vida, mi pingüino precioso, mi ángel más increíble y único en todo el cielo, la estrella más hermosa, brillante y única en todo el universo, la persona más dulce, increíble y sorprendente de todo el universo, mi súper y sorprendente mogliettina, ¡mi todo!",
+  "Pasaremos este día juntos, feliz aniversario, felices 3 años para nosotros mi mogliettina, de parte de tu pequeño topolino viziato!!",
+  "A+G+GIOAN= hoy, por siempre y más allá!!"
+];
 
 const greetingText = document.querySelector('#greeting-container');
 const greetingletter = document.querySelector('#greeting-letter');
 const secretMessageText = document.querySelector('#greeting-container p');
-const letter = document.querySelector('#greeting-letter p');
+const letter = document.querySelector('#greeting-letter div');
 let greetingOpacity = 0;
 let secretMsgOpacity = 0;
 let letterOpacity = 0;
@@ -20,7 +42,7 @@ let flag = false;
 let secretMsg = false;
 let letterMsg = false;
 let letterp = false;
-
+let index = 0;
 let fireworksArr = [];
 let explosionsArr = [];
 
@@ -204,7 +226,7 @@ window.onload = () => {
      
     }
     
-    if (timer > 200 && !letterMsg && !letterp) {
+    if (timer > 300 && !letterMsg && !letterp) {
 
       greetingOpacity = 0;
       greetingText.style.opacity = greetingOpacity;
@@ -215,7 +237,12 @@ window.onload = () => {
       showletter();
       letterp = true;
       showletterMsg();
-
+      //e.stopPropagation();
+      
+      myaudio.play();
+      var text = $('.test').data('text');
+      let groupSize = 5; 
+      typeWriter(text, 0,groupSize);
     }
 
     // if (explosionsArr.length > 0) console.log(explosionsArr[0].opacity);
@@ -241,11 +268,25 @@ window.onload = () => {
   }
   
   startBtn.addEventListener('click', handleClick);
-  startBtn.click();
+ 
   window.addEventListener('resize', function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     console.log(canvas.height)
   })
   
+ 
+  function typeWriter() { 
+      if (index < text.length) {
+            myaudio.play();
+            document.getElementById("test").innerHTML = text[index];
+            index++;
+            setTimeout(typeWriter, 16000); // Adjust the delay as needed
+     
+      } else {
+          //index = 0; // Reset index to loop the text
+          //setTimeout(typeWriter, 9000); // Adjust the delay as needed
+      }
+  }
+
 }
